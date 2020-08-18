617fb66b187af3c85648be0ac999ec304763b188
alicenannini <44375782+alicenannini@users.noreply.github.com>
2020-08-18 17:12:02 +0200

    update generation code, add spark_commands file

--- generateDataset.py
+++ generateDataset.py
@@ -10,29 +10,26 @@ for n in numPoints:
     for k in kClusters:
         for d in dimPoints:
             # open a new file
-            f = open("data/dataset_"+str(n)+"_"+str(k)+"_"+str(d)+".txt", "a")
+            f = open("data/data2_"+str(n)+"_"+str(k)+"_"+str(d)+".txt", "a")
             
             # compute the interval for creating the clusters
-            interval = round(n/(2*k))
+            interval = round(n/k)
             count = 0
-            print("dataset_"+str(n)+"_"+str(k)+"_"+str(d)+"; int: "+str(interval))
+            print("data2_"+str(n)+"_"+str(k)+"_"+str(d)+"; int: "+str(interval))
             
             # compute each point
             for i in range(n):
                 if( (i%interval)==0 and i!=0):
-                    count = count + 2
+                    count = count + 4
                 
                 x = ""
                 for j in range(d):
                     x = x + str( interval*count + random.random()*interval )
-                    x = x + " "
+                    if(j < d-1):
+                        x = x + " "
                 x = x + "\n"
                 # write the new point coordinates in the file
                 f.write(x)
             
             f.close()
 
-# 1000 punti, 7 clusters -> n/(2k)
-# 1 int: 0 - n/2k
-# 2 int: 2*n/2k - 3*n/2k
-# 3 int: 4*n/2k - 5*n/2k
