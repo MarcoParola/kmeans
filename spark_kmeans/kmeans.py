6ece91d8ece3c0b0977c81350eddb22d0a32dcf8
alicenannini <44375782+alicenannini@users.noreply.github.com>
2020-08-19 13:50:01 +0200

    fix bug due to final blank spaces in strings

--- spark_kmeans/kmeans.py
+++ spark_kmeans/kmeans.py
@@ -5,7 +5,9 @@ from random import random
 
 from pyspark import SparkContext
 
-
+"""
+Utility class to save coordinates and weight of each point in the dataset
+"""
 class Sample:
     
     def __init__(self, coord, weight):
@@ -22,9 +24,11 @@ class Sample:
         return self.weight
     
     def toString(self):
-        return self.coord + "\t" + str(self.weight)
+        return str(self.coord) + "\t" + str(self.weight)
 
-
+"""
+Utility function to check if an input string is convertible to float
+"""
 def is_float(s):
     try:
         float(s)
@@ -32,7 +36,9 @@ def is_float(s):
     except ValueError:
         return False
 
-
+"""
+Utility function to convert an input string into an object Sample
+"""
 def sampleFromString(sample):
     param = sample.split("\t")
     if len(param) < 2:
@@ -41,8 +47,10 @@ def sampleFromString(sample):
         weight = param[1]
     return Sample(param[0],weight)
 
-
-# computing distance through Euclidian norm
+"""
+Function that compute distance (through Euclidian norm) between to points
+input: strings that contain the coordinates of the two points
+"""
 def computeDistance(pointX, pointY):
     xx = pointX.split(" ")
     yy = pointY.split(" ")
@@ -59,7 +67,11 @@ def computeDistance(pointX, pointY):
         
     return math.sqrt(dist)
 
-
+"""
+Function that assign the closest cluster to each point
+input: coordinates of a point (as string), array of coordinates (as string) of all cluster centers
+output: index of the center closest to the point
+"""
 def findCenter(point, centers):
     minDist = float("inf")
     for i in range(len(centers)):
@@ -70,24 +82,31 @@ def findCenter(point, centers):
             minIndex = i
     return minIndex
 
-
+"""
+Function that is passed at the 'reduceByKey' transformation, to compute the new centers at each iteration
+input: two Sample.toString() strings
+output: one Sample.toString() string
+"""
 def computeNewCenter(pointX, pointY):
     print("computeNewCenter: "+pointX+", "+pointY)
     
     newCenterCoord = ""
-    sampleX = sampleFromString(str(pointX))
-    sampleY = sampleFromString(str(pointY))
+    sampleX = sampleFromString(pointX)
+    sampleY = sampleFromString(pointY)
     newCenterWeight = sampleX.getWeight() + sampleY.getWeight()
     xx = sampleX.getCoordAsArray()
     yy = sampleY.getCoordAsArray()
+    
     if len(xx) != len(yy):
-        return 0
+        return ""
     
     for i in range(len(xx)):
         if is_float(xx[i]) and is_float(yy[i]):
             x = float(xx[i])*sampleX.getWeight()
             y = float(yy[i])*sampleY.getWeight()
-            newCenterCoord = newCenterCoord + str((x+y)/newCenterWeight) + " "
+            newCenterCoord = newCenterCoord + str((x+y)/newCenterWeight)
+            if i < len(xx)-1:
+                newCenterCoord += " "
     
     return Sample(newCenterCoord, newCenterWeight).toString()
 
@@ -137,10 +156,10 @@ if __name__ == "__main__":
         
         ## SETTING THE SPARK STEPS (transformations and actions)
         # mapping to pairs (key: center_index (int), value: coord + weight (str))
-        clusters = points.map(lambda point: ( findCenter(point,centers), Sample(point,1).toString())).sortByKey()
+        clusters = points.map(lambda point: ( findCenter(point,centers), Sample(point,1).toString() )).sortByKey()
         
         # computing new centers with 'reduceByKey' transformation
-        newCenters = clusters.reduceByKey(computeNewCenter,partitions).sortByKey()
+        newCenters = clusters.reduceByKey(computeNewCenter,partitions)#.sortByKey()
         
         # saving the new centers as array
         arrNewCenters = []
@@ -149,6 +168,7 @@ if __name__ == "__main__":
         
         # checking the stop condition
         diff = checkCenters(centers, arrNewCenters)
+        print("_____ Difference between old and new centers: "+str(diff)+" _____\n\n")
         if diff < THRESHOLD:
             break
         else:
